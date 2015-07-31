7aac6c9de332e501a67abd7c254ff80197723a25
Erwin-Chan <siuchunc@gmail.com>
2015-07-31 17:13:57 +0800

    added pdf export support

--- index.js
+++ index.js
@@ -18,17 +18,36 @@ module.exports = {
 	blocks: {
 		fbq: {
 			process: function(blk) {
-				var para = blk.body;
-				var substr = para.split("$$");
-				var ans = [];
+                if(this.generator === 'website'){
 
-				for(var i = 1; i<substr.length; i++){
-					ans.push(substr[i].split("##", 1));
-						var str = '$$' + ans[i-1].toString() + '##';
-						para = para.replace(str, "<div class='ans'><input type='text' class='form-control input-sm'/></div>");
-				}
+                    var para = blk.body;
+                    var substr = para.split("$$");
+                    var ans = [];
 
-				return "<blockquote class='FBQbox' data-id='"+checksum(blk.body)+"' data-answer='" + JSON.stringify(ans)+ "'>" + para + "<br><button class='btn btn-default btn-sm FBQsubmit'>Submit</button></blockquote>";
+                    for(var i = 1; i<substr.length; i++){
+                        ans.push(substr[i].split("##", 1));
+                        var str = '$$' + ans[i-1].toString() + '##';
+                        para = para.replace(str, "<div class='ans'><input type='text' class='form-control input-sm'/></div>");
+                    }
+
+				    return "<blockquote class='FBQbox' data-id='"+checksum(blk.body)+"' data-answer='" + JSON.stringify(ans)+ "'>" + para + "<br><button class='btn btn-default btn-sm FBQsubmit'>Submit</button></blockquote>";
+                } 
+                else {
+
+                    var para = blk.body;
+                    var substr = para.split("$$");
+                    var ans = [];
+
+                    for(var i = 1; i<substr.length; i++){
+                        ans.push(substr[i].split("##", 1));
+                        var str = '$$' + ans[i-1].toString() + '##';
+                        para = para.replace(str, "______");
+                    }
+
+                    var ansString = ans.join(',');
+
+                    return "<blockquote>"+ para +"<br>ans: "+ansString+"</blockquote>";
+                }
 			}
 		}
 	}
